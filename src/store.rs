5df432f860b43e8cc027cbee8c6cc31e18ac61c3
Jason Pena <jasonpena@awkless.com>
2025-12-15 22:39:06 -0700

    feat: add `crate::store::ClusterStore::open`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/store.rs
+++ src/store.rs
@@ -22,7 +22,7 @@
 //! to this is by listing a cluster as a dependency of another cluster via
 //! the cluster definition file.
 
-use crate::cluster::domain::Cluster;
+use crate::cluster::domain::{Cluster, Git2Cluster, ClusterAccess};
 
 use std::{collections::HashMap, path::{PathBuf, Path}};
 
@@ -32,15 +32,49 @@ pub struct ClusterStore {
 }
 
 impl ClusterStore {
+    /// Construct new cluster store manager.
+    ///
+    /// Will treat target directory as a cluster store. All clusters within
+    /// that path will be opened for management and manipulation.
+    ///
+    /// # Errors
+    ///
+    /// - Return [`ClusterStoreError::Glob`] if cluster entry paths cannot be
+    ///   globbed.
+    /// - Return [`ClusterStoreError::GlobPattern`] if glob pattern for cluster
+    ///   entry is invalid.
+    /// - Return [`ClusterStoreError::ClusterError`] if any cluster entry
+    ///   cannot be opened.
     pub fn open(path: impl AsRef<Path>) -> Result<Self> {
-        todo!();
+        let store_path = path.as_ref().to_path_buf();
+        let pattern = store_path.join("*.git").to_string_lossy().into_owned();
+        let mut clusters = HashMap::new();
+        for entry in glob::glob(pattern.as_str())? {
+            // INVARIANT: The name of a cluster is the directory name minus the .git extension.
+            let path = entry?;
+            let name = path.file_stem().unwrap().to_string_lossy().into_owned();
+            clusters.insert(name, Git2Cluster::try_open(path)?);
+        }
+
+        // TODO: Add checks for valid cluster store structure at some point.
+        Ok(Self {
+            store_path,
+            clusters,
+        })
     }
 }
 
 /// All possible error types for cluster store interaction.
 #[derive(Debug, thiserror::Error)]
 pub enum ClusterStoreError {
-    // TODO: Add errors here!
+    #[error(transparent)]
+    Glob(#[from] glob::GlobError),
+
+    #[error(transparent)]
+    GlobPattern(#[from] glob::PatternError),
+
+    #[error(transparent)]
+    Cluster(#[from] crate::cluster::domain::ClusterError),
 }
 
 /// Friendly result alias :3
