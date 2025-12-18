276bd68b73e2ee6772829f2e1f2e4bd47095b5e0
Jason Pena <jasonpena@awkless.com>
2025-12-17 17:04:45 -0700

    feat: add `Store::undeployed_only_status`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/store.rs
+++ src/store.rs
@@ -268,6 +268,33 @@ impl Store {
         info!("all deployed clusters:\n{}", status);
     }
 
+    #[instrument(skip(self), level = "debug")]
+    pub fn undeployed_only_status(&self) {
+        let state = self.lock_state();
+        if state.clusters.is_empty() {
+            warn!("cluster store is empty");
+            return;
+        }
+
+        let mut status = String::new();
+        for (name, entry) in state.clusters.iter() {
+            if !entry.is_deployed() {
+                let data = format!(
+                    "{} -> {} : {}\n  url: {}\n  include: {:#?}\n  dependencies: {:#?}\n",
+                    name,
+                    entry.definition.settings.work_tree_alias,
+                    entry.definition.settings.description,
+                    entry.definition.settings.url,
+                    entry.definition.settings.include,
+                    entry.definition.dependencies,
+                );
+                status.push_str(data.as_str());
+            }
+        }
+
+        info!("all deployed clusters:\n{}", status);
+    }
+
     #[inline]
     fn lock_state(&self) -> MutexGuard<'_, StoreState> {
         self.state.lock().unwrap()
